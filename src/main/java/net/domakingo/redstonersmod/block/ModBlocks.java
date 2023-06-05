541c0e06cd485badc83434496772a09dfb439a90
domakingo <domakingtheking@gmail.com>
2023-06-05 22:40:04 +0200

    completing the lamp store a variable for texture change pt. 2

--- src/main/java/net/domakingo/redstonersmod/block/ModBlocks.java
+++ src/main/java/net/domakingo/redstonersmod/block/ModBlocks.java
@@ -1,12 +1,12 @@
 package net.domakingo.redstonersmod.block;
 
 import net.domakingo.redstonersmod.RedstonersMod;
-import net.domakingo.redstonersmod.block.custom.FofLampBlock;
+import net.domakingo.redstonersmod.block.custom.LampBlock;
+import net.domakingo.redstonersmod.block.custom.PinkLed;
 import net.domakingo.redstonersmod.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -21,9 +21,13 @@ public class ModBlocks {
     public static DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, RedstonersMod.MOD_ID);
 
-    public static final RegistryObject<Block> FOF_LAMP_BLOCK = registerBlock("fof_lamp_block",
-            () -> new FofLampBlock(BlockBehaviour.Properties.of(Material.BUILDABLE_GLASS)
-                    .strength(0.3f).sound(SoundType.GLASS).lightLevel(state -> state.getValue(FofLampBlock.LIT) ? 15 : 0)));
+    public static final RegistryObject<Block> LAMP_BLOCK = registerBlock("lamp_block",
+            () -> new LampBlock(BlockBehaviour.Properties.of(Material.BUILDABLE_GLASS)
+                    .strength(0.3f).sound(SoundType.GLASS).lightLevel(state -> state.getValue(LampBlock.LIT) ? 15 : 0)));
+
+    public static final RegistryObject<Block> PINK_LED_BLOCK = registerBlock("pink_led_block",
+            () -> new PinkLed(BlockBehaviour.Properties.of(Material.BUILDABLE_GLASS)
+                    .strength(0.3f).sound(SoundType.GLASS).noOcclusion().lightLevel(state -> state.getValue(LampBlock.LIT) ? 15 : 0)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
--- src/main/java/net/domakingo/redstonersmod/block/custom/LampBlock.java
+++ src/main/java/net/domakingo/redstonersmod/block/custom/LampBlock.java
@@ -4,6 +4,9 @@ import javax.annotation.Nullable;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -12,14 +15,17 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.phys.BlockHitResult;
 
-public class FofLampBlock extends Block {
+public class LampBlock extends Block {
     public static final BooleanProperty LIT;
+    public static final IntegerProperty VALUE = IntegerProperty.create("value", 1, 16);
 
-    public FofLampBlock(BlockBehaviour.Properties p_55657_) {
+    public LampBlock(BlockBehaviour.Properties p_55657_) {
         super(p_55657_);
-        this.registerDefaultState((BlockState)this.defaultBlockState().setValue(LIT, false));
+        this.registerDefaultState((BlockState)this.defaultBlockState().setValue(LIT, false).setValue(VALUE, 1));
     }
 
     @Nullable
@@ -49,17 +55,20 @@ public class FofLampBlock extends Block {
     }
 
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_55673_) {
-        p_55673_.add(new Property[]{LIT});
+        p_55673_.add(new Property[]{LIT, VALUE});
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        if (!level.isClientSide) {
+            int currentValue = state.getValue(VALUE);
+            int newValue = currentValue == 16 ? 1 : currentValue + 1;
+            level.setBlock(pos, state.setValue(VALUE, newValue), 2);
+        }
+        return InteractionResult.SUCCESS;
     }
 
     static {
         LIT = RedstoneTorchBlock.LIT;
     }
 }
-
-
-
-
-
-
-
